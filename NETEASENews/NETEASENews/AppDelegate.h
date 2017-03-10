fad508841ce3fe8890d2970563b76edf7a85d30f
史泽东 <chris781245@hotmail.com>
2017-03-10 19:39:35 +0800

    01-项目准备

---
+++ NETEASENews/NETEASENews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NETEASENews
+//
+//  Created by 史泽东 on 2017/3/10.
+//  Copyright © 2017年 史泽东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
