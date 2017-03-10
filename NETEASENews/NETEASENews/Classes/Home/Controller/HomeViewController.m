e2c7c4b4c9ecfd319c27aecb7032e323b0721693
史泽东 <chris781245@hotmail.com>
2017-03-10 21:14:54 +0800

    03--channelView设置

--- NETEASENews/NETEASENews/Classes/Home/Controller/HomeViewController.m
+++ NETEASENews/NETEASENews/Classes/Home/Controller/HomeViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "HomeViewController.h"
+#import "ChannelModel.h"
+#import "ChannelLabel.h"
 
 @interface HomeViewController ()
 
@@ -16,6 +18,9 @@
 
 @property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *flowLayout;
 
+//频道数据源
+@property (nonatomic, strong) NSArray *channelModelArray;
+
 @end
 
 @implementation HomeViewController
@@ -24,14 +29,45 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    self.automaticallyAdjustsScrollViewInsets = NO;
     [self requestChannelData];
 }
 
 - (void) requestChannelData {
     
+    // 记录频道数据源
+    self.channelModelArray = [ChannelModel getChannelModelArray];
     
+    // 遍历频道模型数组，创建对应label
+        // 设置label宽高
+    CGFloat labelWidth = 80;
+    CGFloat labelHeight = 44;
     
+    for (NSInteger i = 0; i < self.channelModelArray.count; i++) {
+        
+        // 获取对应的模型数据
+        ChannelModel *model = self.channelModelArray[i];
+        
+        // 创建label
+        ChannelLabel *channelLabel = [[ChannelLabel alloc] initWithFrame:CGRectMake(i * labelWidth, 0, labelWidth, labelHeight)];
+        
+        channelLabel.text = model.tname;
+        
+        // 设置文字大小和居中方式
+        channelLabel.font = [UIFont systemFontOfSize:15];
+        channelLabel.textAlignment = NSTextAlignmentCenter;
+        
+        [self.channelView addSubview:channelLabel];
+    }
+    
+    // 设置channelView滚动范围
+    self.channelView.contentSize = CGSizeMake(self.channelModelArray.count * labelWidth, 0);
+    
+    // 取消滚动指示器
+    self.channelView.showsVerticalScrollIndicator = NO;
+    self.channelView.showsHorizontalScrollIndicator = NO;
 }
+
 
 
 
