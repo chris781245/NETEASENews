c41af0a55ce6cee08c565a0dba2e143210181daf
史泽东 <chris781245@hotmail.com>
2017-03-10 21:21:39 +0800

    04--News准备

---
+++ NETEASENews/NETEASENews/Classes/Home/Controller/NewsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewController.h
+//  NETEASENews
+//
+//  Created by 史泽东 on 2017/3/10.
+//  Copyright © 2017年 史泽东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewController : UITableViewController
+
+@end
---
+++ NETEASENews/NETEASENews/Classes/Home/Model/NewsModel.h
@@ -0,0 +1,13 @@
+//
+//  NewsModel.h
+//  NETEASENews
+//
+//  Created by 史泽东 on 2017/3/10.
+//  Copyright © 2017年 史泽东. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewsModel : NSObject
+
+@end
